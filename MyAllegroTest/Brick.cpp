de0e7cea91bc87ecd8a8dd370890a80d15b21d87
ahmadbk <ahmadbk99@gmail.com>
2015-04-24 22:18:00 +0200

    Imranucake

--- MyAllegroTest/Brick.cpp
+++ MyAllegroTest/Brick.cpp
@@ -35,54 +35,45 @@ bool Brick::detectCollisionVertical(const int ball_x, const int ball_y, const in
 {
    int temp_dy = abs(ball_dy);
 
-   if (super == true)
-   {
    if ((ball_y + BALL_SIZE_RADIUS >= locationY) && (ball_y + BALL_SIZE_RADIUS <= locationY + temp_dy) && (ball_x /*+ BALL_SIZE_RADIUS*/ > locationX) && (ball_x /*- BALL_SIZE_RADIUS*/ < locationX + sizeX))
-	   {
+   {
+	   if (super)
 		   superLevel--;
+
 		return true;
-	   }
+   }
    else if ((ball_y - BALL_SIZE_RADIUS <= locationY + sizeY) && (ball_y - BALL_SIZE_RADIUS >= locationY + sizeY - temp_dy) && (ball_x /*+ BALL_SIZE_RADIUS*/ > locationX) && (ball_x /*- BALL_SIZE_RADIUS*/ < locationX + sizeX))
-	   {
+   {
+	   if (super)
 		   superLevel--;
-		return true;
-	   }
+
+	   return true;
    }
    else
-   {
-	   if ((ball_y + BALL_SIZE_RADIUS >= locationY) && (ball_y + BALL_SIZE_RADIUS <= locationY + temp_dy) && (ball_x /*+ BALL_SIZE_RADIUS*/ > locationX) && (ball_x /*- BALL_SIZE_RADIUS*/ < locationX + sizeX))
-		   return true;
-	   else if ((ball_y - BALL_SIZE_RADIUS <= locationY + sizeY) && (ball_y - BALL_SIZE_RADIUS >= locationY + sizeY - temp_dy) && (ball_x /*+ BALL_SIZE_RADIUS*/ > locationX) && (ball_x /*- BALL_SIZE_RADIUS*/ < locationX + sizeX))
-		   return true;
-   }
-		return false;
+	   return false;
 };
 
 bool Brick::detectCollisionHorizontal(const int ball_x, const int ball_y, const int ball_dx, const int ball_dy, const int BALL_SIZE_RADIUS)
 {
 	int temp_dx = abs(ball_dx);
 	int temp_dy = abs(ball_dy);
-	if (super == true)
+
+	if ((ball_x + BALL_SIZE_RADIUS >= locationX) && (ball_x + BALL_SIZE_RADIUS <= locationX + temp_dx) && (ball_y + BALL_SIZE_RADIUS > locationY + temp_dy) && (ball_y - BALL_SIZE_RADIUS < locationY + sizeY))
 	{
-		if ((ball_x + BALL_SIZE_RADIUS >= locationX) && (ball_x + BALL_SIZE_RADIUS <= locationX + temp_dx) && (ball_y + BALL_SIZE_RADIUS > locationY + temp_dy) && (ball_y - BALL_SIZE_RADIUS < locationY + sizeY))
-		{
+		if (super)
 			superLevel--;
-	   return true;
-		}
-		else if ((ball_x - BALL_SIZE_RADIUS <= locationX + sizeX) && (ball_x + BALL_SIZE_RADIUS >= locationX + sizeX - temp_dx) && (ball_y + BALL_SIZE_RADIUS > locationY + temp_dy) && (ball_y - BALL_SIZE_RADIUS < locationY + sizeY))
-		{
+
+		return true;
+	}
+	else if ((ball_x - BALL_SIZE_RADIUS <= locationX + sizeX) && (ball_x + BALL_SIZE_RADIUS >= locationX + sizeX - temp_dx) && (ball_y + BALL_SIZE_RADIUS > locationY + temp_dy) && (ball_y - BALL_SIZE_RADIUS < locationY + sizeY))
+	{
+		if (super)
 			superLevel--;
-	   return true;
-		}
+
+		return true;
 	}
 	else
-	{
-		if ((ball_x + BALL_SIZE_RADIUS >= locationX) && (ball_x + BALL_SIZE_RADIUS <= locationX + temp_dx) && (ball_y + BALL_SIZE_RADIUS > locationY + temp_dy) && (ball_y - BALL_SIZE_RADIUS < locationY + sizeY))
-			return true;
-		else if ((ball_x - BALL_SIZE_RADIUS <= locationX + sizeX) && (ball_x + BALL_SIZE_RADIUS >= locationX + sizeX - temp_dx) && (ball_y + BALL_SIZE_RADIUS > locationY + temp_dy) && (ball_y - BALL_SIZE_RADIUS < locationY + sizeY))
-			return true;
-	}
-	   return false;
+		return false;
 };
 
 
