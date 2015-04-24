61ee04d0e606bc142606031a1494549fb4b5708d
imranparuk <parukboy@gmail.com>
2015-04-24 22:08:48 +0200

    collision

--- MyAllegroTest/Brick.cpp
+++ MyAllegroTest/Brick.cpp
@@ -42,9 +42,9 @@ bool Brick::detectCollisionHorizontal(const int ball_x, const int ball_y, const 
 	int temp_dx = abs(ball_dx);
 	int temp_dy = abs(ball_dy);
 
-	if ((ball_x + BALL_SIZE_RADIUS >= locationX) && (ball_x + BALL_SIZE_RADIUS <= locationX + temp_dx) && (ball_y + BALL_SIZE_RADIUS > locationY + temp_dy) && (ball_y - BALL_SIZE_RADIUS < locationY + sizeY))
+	if ((ball_x + BALL_SIZE_RADIUS >= locationX) && (ball_x + BALL_SIZE_RADIUS <= locationX + temp_dx) && (ball_y + BALL_SIZE_RADIUS >= locationY) && (ball_y - BALL_SIZE_RADIUS <= locationY + sizeY))
 	   return true;
-	else if ((ball_x - BALL_SIZE_RADIUS <= locationX + sizeX) && (ball_x + BALL_SIZE_RADIUS >= locationX + sizeX - temp_dx) && (ball_y + BALL_SIZE_RADIUS > locationY + temp_dy) && (ball_y - BALL_SIZE_RADIUS < locationY + sizeY))
+	else if ((ball_x - BALL_SIZE_RADIUS <= locationX + sizeX) && (ball_x + BALL_SIZE_RADIUS >= locationX + sizeX - temp_dx) && (ball_y + BALL_SIZE_RADIUS >= locationY) && (ball_y - BALL_SIZE_RADIUS <= locationY + sizeY))
 	   return true;
 	else
 	   return false;
