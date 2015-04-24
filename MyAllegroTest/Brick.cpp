5c979b694c3046077a032c4b3dc53b6ab184d2b7
imranparuk <parukboy@gmail.com>
2015-04-24 22:11:25 +0200

    yaa

--- MyAllegroTest/Brick.cpp
+++ MyAllegroTest/Brick.cpp
@@ -84,6 +84,34 @@ bool Brick::detectCollisionHorizontal(const int ball_x, const int ball_y, const 
 	}
 	   return false;
 };
+
+
+/*bool Brick::detectCollisionVertical(const int ball_x, const int ball_y, const int ball_dx, const int ball_dy, const int BALL_SIZE_RADIUS)
+{
+   int temp_dy = abs(ball_dy);
+
+   if ((ball_y + BALL_SIZE_RADIUS >= locationY) && (ball_y + BALL_SIZE_RADIUS <= locationY + temp_dy) && (ball_x > locationX) && (ball_x < locationX + sizeX))
+	  return true;
+   else if ((ball_y - BALL_SIZE_RADIUS <= locationY + sizeY) && (ball_y - BALL_SIZE_RADIUS >= locationY + sizeY - temp_dy) && (ball_x  > locationX) && (ball_x  < locationX + sizeX))
+	   return true;
+   else
+	   return false;
+
+};
+
+bool Brick::detectCollisionHorizontal(const int ball_x, const int ball_y, const int ball_dx, const int ball_dy, const int BALL_SIZE_RADIUS)
+{
+	int temp_dx = abs(ball_dx);
+	int temp_dy = abs(ball_dy);
+
+	if ((ball_x + BALL_SIZE_RADIUS >= locationX) && (ball_x + BALL_SIZE_RADIUS <= locationX + temp_dx) && (ball_y + BALL_SIZE_RADIUS >= locationY) && (ball_y - BALL_SIZE_RADIUS <= locationY + sizeY))
+		return true;
+	else if ((ball_x - BALL_SIZE_RADIUS <= locationX + sizeX) && (ball_x + BALL_SIZE_RADIUS >= locationX + sizeX - temp_dx) && (ball_y + BALL_SIZE_RADIUS >= locationY) && (ball_y - BALL_SIZE_RADIUS <= locationY + sizeY))
+		return true;
+	else
+		return false;
+
+}; */
 bool Brick::isDestroyed()
 {
 	return destroyed;
