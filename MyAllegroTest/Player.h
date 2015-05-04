13853dbbf7cf762ad5ca455ca7370c163d86410c
mziadawood <mziadawood@gmail.com>
2015-05-04 17:41:46 +0200

    Header Class Added

--- MyAllegroTest/Player.h
+++ MyAllegroTest/Player.h
@@ -1,17 +1,36 @@
 #pragma once
+#include <stdio.h>
+#include"allegro5/allegro.h"
+#include <iostream>
+
 class Player
 {
+	const int SCREEN_W = 640;
+	const int SCREEN_H = 480;
 
 private:
-	int locationX;
-	int locationY;
+	int locationX = SCREEN_W / 2.0 - sizeX / 2.0;
+	int locationY = SCREEN_H - 70;
 
 	int sizeX;
 	int sizeY;
 
+	int centre = sizeX / 2;
 
+	ALLEGRO_BITMAP *player = NULL;
+	
 public:
-	Player();
+	Player(int sX, int sY)
+	{ 
+		sizeX = sX; 
+		sizeY = sY; 
+
+		player = al_create_bitmap(sizeX, sizeY);//actual player made
+		if (!player)
+		{
+			fprintf(stderr, "Failed to create player bitmap!\n");
+		}
+	};
 	~Player();
 
 	int getLocX();
